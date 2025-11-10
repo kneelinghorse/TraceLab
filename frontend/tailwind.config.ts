434128e0d47b40a01ca337cc2716985dd7cf2761
System Systems <systems@example.com>
2025-11-10 10:57:26 -0600

    Include features directory in Tailwind build

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/features/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/lib/**/*.{js,ts,jsx,tsx}",
   ],
